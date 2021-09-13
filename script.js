784312851136c599aafb192775270bbfcb76c930
jay05thakare <74776539+jay05thakare@users.noreply.github.com>
2021-09-13 12:17:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+const container = document.querySelector(".container")
+
+Array.from(document.querySelectorAll(".navbar-icon")).forEach(item => {
+    item.addEventListener('click', () => {
+        container.classList.toggle("change");
+    });
+});
+
+
+const colors = ["#6495ed", "#7fffd4", "#ffa07a", "#f08080", "#afeeee"];
+
+let i = 0;
+
+Array.from(document.querySelectorAll(".nav-link")).forEach(item => {
+    item.style.cssText = `background-color: ${colors[i++]}`;
+});
+
+
+Array.from(document.querySelectorAll(".navigation-button")).forEach(item => {
+    item.onclick = () => {
+        item.parentElement.parentElement.classList.toggle("change");
+    };
+});
